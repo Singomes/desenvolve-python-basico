92566ab439ef22485f5aeda75d9937eb5037bbee
Singomes <simone.gomes@pdbomdespacho.com.br>
2026-03-23 13:39:32 -0300

    Delete modulo 2/aula4_questao2.py

--- modulo 2/aula4_questao2.py
+++
@@ -1,8 +0,0 @@
-fahrenheit = int(input("Digite a temperatura em Fahrenheit: "))
-
-celsius = (fahrenheit - 32) * 5/9
-
-print(f"{fahrenheit} graus Fahrenheit são {int(celsius)} graus Celsius.")
-
-
-
